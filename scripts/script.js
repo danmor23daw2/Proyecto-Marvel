3a1f76f5ff4f5dd82504f24c64cbe0bf880c8743
danmor23daw2 <15586175@fje.edu>
2024-01-10 22:21:09 +0100

    llamadas a API compactas

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,32 @@
+let hulkId = 1009351;
+let thorId = 1009664;
+
 const ts = 1;
 const publickey = "dd2ca7aa45a3d7a09763bb0e98d24f39";
 const hash = "cb78c8fab02c168b9097e47bbe10cbf5";
 const limit = "limit=8";
 const randomOffset = `offset=${Math.round(Math.random() * 100)}`;
 
-function getSuperheroInfo() {
-    const characterId = "1009351";
+function getSuperheroInfo(characterId) {
     const url = `http://gateway.marvel.com/v1/public/characters/${characterId}/comics?${limit}&${randomOffset}&ts=${ts}&apikey=${publickey}&hash=${hash}`;
     
-    var characterXhr = new XMLHttpRequest();
+    let characterXhr = new XMLHttpRequest();
 
     characterXhr.open('GET', url, true);
 
     characterXhr.onload = function () {
         if (characterXhr.status >= 200 && characterXhr.status < 300) {
-            var characterResponse = JSON.parse(characterXhr.responseText);
+            let characterResponse = JSON.parse(characterXhr.responseText);
 
-            var superhero = characterResponse.data.results[0];
+            let superhero = characterResponse.data.results[0];
 
-            var superheroInfoContainer = document.getElementById('superheroInfo');
+            let superheroInfoContainer = document.getElementById('superheroInfo');
             superheroInfoContainer.innerHTML = `
             <br><br><br><br>
-                <img class="img2"src="${superhero.thumbnail.path}.${superhero.thumbnail.extension}" alt="${superhero.name}">
+                <img class="img2" src="${superhero.thumbnail.path}.${superhero.thumbnail.extension}" alt="${superhero.name}">
             `;
 
-            var comics = superhero.comics.items;
+            let comics = superhero.comics.items;
             comics.slice(0, 6).forEach(function (comic) {
                 superheroInfoContainer.innerHTML += `<p>${comic.name}</p>`;
             });
@@ -38,43 +40,4 @@ function getSuperheroInfo() {
     };
 
     characterXhr.send();
-}
-function getSuperheroInfo2() {
-    const characterId = "1009664";
-    const url = `http://gateway.marvel.com/v1/public/characters/${characterId}/comics?${limit}&${randomOffset}&ts=${ts}&apikey=${publickey}&hash=${hash}`;
-    
-    var characterXhr = new XMLHttpRequest();
-
-    characterXhr.open('GET', url, true);
-
-    characterXhr.onload = function () {
-        if (characterXhr.status >= 200 && characterXhr.status < 300) {
-            var characterResponse = JSON.parse(characterXhr.responseText);
-
-            var superhero = characterResponse.data.results[0];
-
-            var superheroInfoContainer = document.getElementById('superheroInfo');
-            superheroInfoContainer.innerHTML = `
-            <br><br><br><br>
-                <img class="img2"src="${superhero.thumbnail.path}.${superhero.thumbnail.extension}" alt="${superhero.name}">
-                <p>${superhero.description}</p>
-            `;
-
-            var comics = superhero.comics.items;
-            comics.slice(0, 5).forEach(function (comic) {
-                superheroInfoContainer.innerHTML += `<p>${comic.name}</p>`;
-            });
-        } else {
-            console.error('Error en la solicitud. Código de estado:', characterXhr.status);
-        }
-    };
-
-    characterXhr.onerror = function () {
-        console.error('Error de red al intentar realizar la solicitud.');
-    };
-
-    characterXhr.send();
-}
-function md5(value) {
-    
 }
